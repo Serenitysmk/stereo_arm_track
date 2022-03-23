195e85803e1a1967ab3d2519d05e4acf5666aae7
Mengkun She <mshe@geomar.de>
2022-03-23 12:22:41 +0100

    initial code

---
+++ stereo_arm_track/test_bed.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <base/camera.h>
+
+int main(int argc, char** argv) {
+  std::cout << "Hello World" << std::endl;
+
+  colmap::Camera camera;
+  
+  return EXIT_SUCCESS;
+}
