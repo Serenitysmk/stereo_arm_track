e27f60a997e87bb25c40715603cd4b3dee5e6c21
Mengkun She <mshe@geomar.de>
2022-03-23 13:14:04 +0100

    get HuaRay SDK running

--- stereo_arm_track/test_bed.cc
+++ stereo_arm_track/test_bed.cc
@@ -2,10 +2,20 @@
 
 #include <base/camera.h>
 
+#include <IMVApi.h>
+
 int main(int argc, char** argv) {
   std::cout << "Hello World" << std::endl;
 
   colmap::Camera camera;
   
+  int ret = IMV_OK;
+  unsigned int cameraIndex = 0;
+  IMV_HANDLE devHandle = NULL;
+
+  IMV_DeviceList deviceInfoList;
+  ret = IMV_EnumDevices(&deviceInfoList, interfaceTypeAll);
+  
+  std::cout << "Number of devices: " << deviceInfoList.nDevNum << std::endl;
   return EXIT_SUCCESS;
 }
